5d13a0c25272ee72c046656ff4d2bd0d2a0d93b1
lucasbernardol <jose.lukass03@gmail.com>
2022-07-08 15:49:14 -0300

    feat: add Feedback/modal component

--- public/static/scripts/home.scripts.js
+++ public/static/scripts/home.scripts.js
@@ -1,9 +1,14 @@
 const spinnerElement = document.querySelector('[data-id="spinner"]');
 const outputElement = document.querySelector('[data-id="output"]');
 
-const API_PUBLIC_KEY = 'baron-18818k4e8l4yxcb6n';
+const feedbackForm = document.querySelector('[data-id="form"]');
 
-const BASE_URL = '/api/v1/hits';
+const checkboxElements = document.querySelectorAll('input[type="checkbox"]');
+const inputElments = document.querySelectorAll('input');
+const textareaElements = document.querySelectorAll('textarea');
+
+const API_PUBLIC_KEY = 'baron-18818k4e8l4yxcb6n';
+const BASE_URL = '/api/v1';
 
 const delay = (delayInMilliseconds = 500) => {
   return new Promise((resolve, _) => {
@@ -24,7 +29,7 @@ class ApiRequest {
    * @param {RequestInit} options - Fetch options
    */
   async fetch(url = this._base_url, options) {
-    const mergedOptions = { ...options, method: 'PATCH' };
+    const mergedOptions = { method: 'PATCH', ...options };
 
     const apiResponse = await fetch(url, mergedOptions);
 
@@ -33,6 +38,99 @@ class ApiRequest {
     return {
       responseBodyInJSON,
     };
+  }
+}
+
+class Feedback {
+  /**
+   * @param {{
+   *  formElement: HTMLFormElement;
+   *  checkboxs: HTMLElement[];
+   *  textareaElements: HTMLTextAreaElement[];
+   *  inputElments: HTMLInputElement[];
+   *  apiInstance: ApiRequest,
+   * }} options
+   **/
+  constructor({
+    formElement,
+    checkboxs,
+    inputElments,
+    textareaElements,
+    apiInstance,
+  }) {
+    this.checkboxs = Array.from(checkboxs);
+
+    this.inputs = Array.from(inputElments);
+    this.textareas = Array.from(textareaElements);
+
+    this.formElement = formElement;
+    this.api = apiInstance;
+  }
+
+  values() {
+    const fieldsArray = [...this.checkboxs, ...this.inputs, ...this.textareas];
+
+    const fields = fieldsArray.reduce((accumulator, element) => {
+      let value = null;
+
+      const HTML_ELEMENT_TYPE = element.getAttribute('type');
+
+      switch (HTML_ELEMENT_TYPE) {
+        case 'checkbox':
+          value = element.checked;
+          break;
+
+        default:
+          value = element.value || null;
+      }
+
+      const key = element.getAttribute('name');
+
+      return { ...accumulator, [key]: value };
+    }, {});
+
+    return fields;
+  }
+
+  clearFields() {
+    const fieldsArray = [...this.textareas, ...this.inputs];
+
+    fieldsArray.forEach((element) => (element.value = null));
+  }
+
+  async execute(event) {
+    const { submitter: buttonElement } = event;
+
+    const fields = this.values();
+
+    try {
+      buttonElement.disabled = true;
+
+      const url = `${BASE_URL}/feedbacks`;
+
+      const { responseBodyInJSON } = await this.api.fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(fields),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+
+      console.log({ ...responseBodyInJSON });
+    } catch (error) {
+    } finally {
+      this.clearFields();
+
+      buttonElement.disabled = false;
+    }
+  }
+
+  init() {
+    this.formElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      this.execute(event);
+    });
   }
 }
 
@@ -72,7 +170,7 @@ class HitView {
 
   async execute() {
     try {
-      const url = `${BASE_URL}/up/${API_PUBLIC_KEY}`;
+      const url = `${BASE_URL}/hits/up/${API_PUBLIC_KEY}`;
 
       const { responseBodyInJSON } = await this.apiInstance.fetch(url, {
         method: 'PATCH',
@@ -104,7 +202,17 @@ async function App() {
     spinnerElement,
   });
 
+  const form = new Feedback({
+    formElement: feedbackForm,
+    checkboxs: checkboxElements,
+    inputElments,
+    textareaElements,
+    apiInstance,
+  });
+
   await view.execute();
+
+  form.init();
 }
 
 window.addEventListener('load', () => App());
