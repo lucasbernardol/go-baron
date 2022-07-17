f8eb86928473037e693694ffa678098df9d55ac6
lucasbernardol <jose.lukass03@gmail.com>
2022-07-17 17:41:14 -0300

    fix: update API/server port

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import 'dotenv/config';
 import { Application } from './api/config/express.config';
 import { Server } from './api/config/server.config';
 
+import { env } from './api/config/environment.config';
+
 /** Bootstrap */
 Server.init(Application, {
   allowNativeHTTPServer: true,
 }).listen({
-  port: 3333,
+  port: env.PORT,
   // serverListenCallback: (listen) => {
   //   console.log(listen);
   // },
