90e46a7604fc4061690bd5faeca2383e231f4f03
lucasbernardol <jose.lukass03@gmail.com>
2022-07-18 11:05:56 -0300

    feat: add global rate limit

--- src/api/core/routes/v1/proxy.routes.ts
+++ src/api/core/routes/v1/proxy.routes.ts
@@ -1,15 +1,25 @@
 import { Router } from 'express';
 
 import { MainController } from '../../app/controllers/main.controllers';
+import { createLimiter } from '../../middlewares/v1/limiter.middleware';
 
 import { HitRouter } from './hits.routes';
 import { FeedbackRouter } from './feedbacks.routes';
-
 import { HealthRouter } from './health.routes';
 
 const routes = Router();
 
 const controller = new MainController();
+
+/**
+ * - Express rate limiter/global request limiter/per IP.
+ */
+const limiter = createLimiter({
+  windowMs: 1 * 60 * 60 * 1000, // 1 hour
+  max: 1500,
+});
+
+routes.use(limiter);
 
 /**
  * @route "/"
